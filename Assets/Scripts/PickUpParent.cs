2eb5378047b026323939bb9a6839520c28496133
Steven Alexander <steven.william.alexander@googlemail.com>
2016-05-09 12:19:56 +0100

    added touch/press input to allow picking up ball

---
+++ Assets/Scripts/PickUpParent.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickUpParent : MonoBehaviour {
+
+    SteamVR_TrackedObject trackedObj;
+    SteamVR_Controller.Device device;
+
+
+    void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	void FixedUpdate () {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'Touch' on the trigger");
+        }
+
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You activated 'TouchDown' on the trigger");
+        }
+
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You activated 'TouchUp' on the trigger");
+        }
+
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding 'Press' on the trigger");
+        }
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You activated 'PressDown' on the trigger");
+        }
+
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You activated 'PressUp' on the trigger");
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        Debug.Log("You have collider with: " + col.name + " and activated OnTriggerStay");
+            
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have collided with " + col.name + " while holding down touch");
+
+            col.attachedRigidbody.isKinematic = true;
+            col.gameObject.transform.SetParent(gameObject.transform);
+        }
+
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have released touch while colliding with " + col.name);
+            
+            col.gameObject.transform.SetParent(null);
+            col.attachedRigidbody.isKinematic = false;
+        }
+    }
+}
